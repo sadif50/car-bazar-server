efe52dabe68465bdca901e9f5bb4eb647ef70781
S. M. Sujan <smsujon50@gmail.com>
2022-11-24 15:37:19 +0600

    Make server for the first time successfuly.

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Car Bazar Server Running!');
+})
+
+app.listen(port, ()=>{
+    console.log(`Car Bazar Server Running on ${port}`);
+})
